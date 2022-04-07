00a47750feec1d6f73011cbf05ee04814abb7abc
vudu79 <vudu1979@gmail.com>
2022-04-07 17:57:43 +1200

    все готово до корзины

---
+++ database/migrations/2022_04_06_094415_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id');
+            $table->string('code');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->double('price')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};

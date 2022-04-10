c1febb7fe86257458c2bcb6916e27641e76fad4a
vudu79 <vudu1979@gmail.com>
2022-04-11 11:37:20 +1200

    закончил заказы

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -11,6 +11,11 @@ class AdminController extends Controller
     public function index()
     {
         $orders = Order::where('status',1)->get();
-        return view('admin.orders', compact('orders'));
+        return view('admin.orders.orders', compact('orders'));
+    }
+
+    public function show(Order $order)
+    {
+        return view('admin.orders.show', compact('order'));
     }
 }
--- database/migrations/2022_04_10_095853_add_column_user_to_orders_table.php
+++ database/migrations/2022_04_10_095853_add_column_user_to_orders_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('categories', function (Blueprint $table) {
-            $table->dropColumn('image');
+        Schema::table('orders', function (Blueprint $table) {
+            //
         });
     }
 
@@ -25,8 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('categories', function (Blueprint $table) {
-            $table->string('image')->nullable();
+        Schema::table('orders', function (Blueprint $table) {
+            //
         });
     }
 };

1a7586bd9a1886fc3b530f1da07bc82f00e25ba3
vudu79 <vudu1979@gmail.com>
2022-04-12 16:21:16 +1200

    пытаюсь залить фотки на github

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Auth;
+use  Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
